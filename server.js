d32bfc293a33de3cd9efa31edb655099b6602e49
yixizhang <chongchong1110@gmail.com>
2016-03-25 18:38:44 -0700

    finish conversation feature

--- server.js
+++ server.js
@@ -16,25 +16,29 @@ var server = app.listen(8888,function () {
 
 var io = require('socket.io').listen(server);
 var all_user = [];
+var all_messages = [];
 io.sockets.on('connection', function(socket){
 	socket.on('got_a_new_user', function(data){
 		var user = {};
 		user[socket.id] = data.user;
-		// var user_id = socket.id;
-		// console.log(user_id)
 		all_user.push(user);
 		console.log(all_user);
-		socket.emit('all_user', {user:all_user})
+		socket.emit('all_user', {user:all_user});
 		socket.broadcast.emit('new_user',{user: [user]});
+		socket.emit('all_messages',{messages: all_messages })
+	});
+
+	socket.on('get_a_message', function(data){
+		console.log(data);
+		var message = {};
+		message[data.name]=data.message;
+		all_messages.push(message);
+		io.emit('a_new_message',{message:message});
 	})
+
 	socket.on('disconnect',function(){
 		var i = all_user.indexOf(socket.id);
 		all_user.splice(i,1);
 		io.emit('exit_user', {exit:socket.id});
 	})
-	// socket.on('connected', function(socket) {
-	// 	console.log('got message');
-	// 	console.log('all user', all_user);
-	// 	io.emit('all_user', {user:all_user})
-	// })
 })
